1b76895155897efda6b928f086330a535c26478a
Andrii Chyryk <achyryk86@gmail.com>
2019-12-17 01:57:34 +0200

    add style.display.none prop to the parent div #block-choice in the same EvList

--- script/script.js
+++ script/script.js
@@ -5,11 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const freelancer = document.getElementById('freelancer');
     const clientBlock = document.querySelector('#block-customer');
     const freelancerBlock = document.querySelector('#block-freelancer');
+    const choiceBlock = document.querySelector('#block-choice');
+
     
     client.addEventListener('click', () => {
         clientBlock.style.display = 'block';
+        choiceBlock.style.display = 'none';
     });
     freelancer.addEventListener('click', () => {
         freelancerBlock.style.display = 'block';
+        choiceBlock.style.display = 'none';
     });
 });
